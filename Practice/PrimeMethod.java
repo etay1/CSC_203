ec419c5611f3410274d3b8714610e0225a8d8ffc
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-01-29 17:50:55 -0500

    Update PrimeMethod.java

--- Practice/PrimeMethod.java
+++ Practice/PrimeMethod.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class RandomShit {
+public class Prime {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Prime number check: ");
