a94de4c28b1c45405dd82a19bb9f0cf42c286c06
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-01-29 19:26:28 -0500

    Update QuadraticFormula.java

--- Lab 1/QuadraticFormula.java
+++ Lab 1/QuadraticFormula.java
@@ -6,7 +6,11 @@ public class Exercise3 {
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
+        double comp = QuadraticCalc(a,b,c);
+        System.out.println("The quadratic " + comp);
+    }
+    public static double QuadraticCalc (double a, double b, double c){
         double x = ((Math.sqrt(Math.pow(b,2) - (4*a*c))) - b) / (2*a);
-        System.out.println("Computation of x is : " + x);
+        return x;
     }
 }
