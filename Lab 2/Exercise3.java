37bbd04cdc907a82e88b56609bbf125c45c883dd
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-02-02 16:12:42 -0500

    Update Exercise3.java

--- Lab 2/Exercise3.java
+++ Lab 2/Exercise3.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = input.nextInt();
+        boolean leap = false;
 
+        if (year % 4 == 0) {
+            leap = true;
+        } else if (year % 4 == 0 && year % 400 == 0) {
+            leap = true;
+        } else if (year % 100 == 0) {
+            leap = false;
+        }
+
+        if (leap)
+            System.out.println(year + " is a leap year");
+        else
+            System.out.println(year + " is a common year");
+
+    }
+
+}
