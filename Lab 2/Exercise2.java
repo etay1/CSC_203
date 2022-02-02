e4c1dada97ed08f68f49dcd617da4ff0c65a22f1
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-02-02 16:13:11 -0500

    Update Exercise2.java

--- Lab 2/Exercise2.java
+++ Lab 2/Exercise2.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int user = input.nextInt();
 
+        if (user % 5 == 0 && user % 2 == 0) {
+            System.out.println("HiFive \n HiEven");
+        } else if (user % 5 == 0)
+            System.out.println("HiFive");
+        else if (user % 2 == 0)
+            System.out.println("HiEven");
+        else
+            System.out.println("Neither");
+
+    }
+}
