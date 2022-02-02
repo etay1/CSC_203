c33da5537116802d235ddbc8f694ded841d1d97e
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-02-02 16:13:40 -0500

    Update Exercise1.java

--- Lab 2/Exercise1.java
+++ Lab 2/Exercise1.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter weight (lbs): ");
+        double lbs = input.nextDouble();
+        System.out.println("Enter height (in): ");
+        double inch = input.nextDouble();
+        double kilo = lbs * (.45359237);
+        double meter = inch * (.0254);
+        double bmi = BMI(kilo,meter);
+        System.out.println("Your BMI is: " + bmi);
+    }
+    public static double BMI (double w, double h) {
+        double x;
+        x = (w / Math.pow(h,2));
+        return x;
+    }
+}
