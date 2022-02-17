321f42fb8b11faceb5394065ee6f03f73e48dc64
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-02-16 23:33:10 -0500

    Update Exercise2.java

--- Lab 4/Exercise2.java
+++ Lab 4/Exercise2.java
@@ -1,3 +1,22 @@
+//Sample for stopwatch class:
+import java.util.Scanner;
+public class test_StopWatch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StopWatch sw = new StopWatch(); //class=stopwatch + object=sw
+
+        System.out.println("Enter the number of times you want the empty loop to run (integer)");
+        int n = input.nextInt();
+
+        sw.start();//starts stop watch
+        for (int i = 0; i < n; i++) //loop size based on user input
+            sw.stop();//ends stop watch
+
+        System.out.print("The loop took " + sw.getElapsedTime() + " nanoseconds to complete."); //get method
+    }
+}
+
+//Exercise 2 assignment requirements:
 class StopWatch {
     public long startTime;
     public long endTime;
@@ -13,25 +32,5 @@ class StopWatch {
     }
     public long getElapsedTime() {
         return endTime - startTime;
-    }
-}
-
-//Sample test below
-
-import java.util.Scanner;
-public class test_StopWatch {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        StopWatch sw = new StopWatch(); //class=stopwatch + object=sw
-
-        System.out.println("Enter the number of times you want the loop to run (integer)");
-        int n = input.nextInt();
-        System.out.println("The loop will run " + n + " times.");
-
-        sw.start();//starts stop watch
-        for (int i = 0; i < n; i++) //loop size based on user input
-        sw.stop();//ends stop watch
-
-        System.out.print("The loop took " + sw.getElapsedTime() + " nanoseconds to complete."); //get method
     }
 }
