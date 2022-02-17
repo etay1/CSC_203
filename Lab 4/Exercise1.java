8a20e500f28aedf2529b7f487f49f2a397cf8b57
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-02-16 22:21:33 -0500

    Create Exercise1.java

---
+++ Lab 4/Exercise1.java
@@ -0,0 +1,37 @@
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(4,40);
+        Rectangle rectangle2 = new Rectangle(3.5,35.9);
+        System.out.println("Width: " + rectangle1.width + " | Height: " + rectangle1.height + " | Area: " + rectangle1.getArea() + " | Perimeter: " + rectangle1.getPerimeter());
+        System.out.println("Width: " + rectangle2.width + " | Height: " + rectangle2.height + " | Area: " + rectangle2.getArea() + " | Perimeter: " + rectangle2.getPerimeter());
+    }
+}
+
+class Rectangle {
+    double width; // data field
+    double height; // data field
+
+    Rectangle() { // no arg constructor
+        width = 1.0; //not being used
+        height = 1.0; //just added due to exercise requirements
+    }
+
+    Rectangle(double newWidth, double newHeight) { //specify variables
+        width = newWidth;
+        height = newHeight;
+    }
+
+    double getArea() { //method for area
+        return height * width;
+    }
+
+    double getPerimeter() { // method for perimeter
+        return (2 * width) + (2 * height);
+    }
+
+}
+
+
+
+
+
