c3b08bd2bf07e5b430409e9be0482c795096fd67
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-03-24 23:45:50 -0400

    Update Exercise2.java

--- Lab 3/Exercise2.java
+++ Lab 3/Exercise2.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number to display n many lines: ");
+        int n = input.nextInt();
+        pattern(n);
+    }
 
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i;j++)
+                System.out.print("* ");
+            System.out.println("");
+        }
+    }
+
+}
+/*
+Sample Exercise2:
+Enter number to display n many lines: 
+5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
